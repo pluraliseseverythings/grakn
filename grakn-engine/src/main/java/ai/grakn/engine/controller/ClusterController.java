f36e912df9a5fbab1f03dc7c5b986793109ae275
lolski <soulmagic13@gmail.com>
2017-10-02 22:48:06 +0700

    adding a basic grakn server cluster init and grakn server cluster join. cluster name cannot be changed without wiping the keyspace

--- grakn-engine/src/main/java/ai/grakn/engine/controller/ClusterController.java
+++ grakn-engine/src/main/java/ai/grakn/engine/controller/ClusterController.java
@@ -1,3 +1,21 @@
+/*
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs Limited
+ *
+ * Grakn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Grakn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
 package ai.grakn.engine.controller;
 
 import mjson.Json;
@@ -9,6 +27,14 @@ import spark.Service;
 // - master: curl s_host/cluster '{ master_host: m_host }'
 // - slave:
 //    - 
+
+/**
+ * <p>
+ *     TODO
+ * </p>
+ *
+ * @author Ganeshwara Herawan Hananda
+ */
 
 // TODO: must be extracted into a plugin
 // TODO:
@@ -25,7 +51,7 @@ public class ClusterController {
         Json requestBody = Json.read(request.body());
         String host = requestBody.at("host").asString();
         int port = requestBody.at("port").asInteger();
-        ClusterManager.add(); // TODO:
+//        ClusterManager.add(); // TODO:
 
         return Json.nil();
     }
@@ -40,16 +66,16 @@ public class ClusterController {
 
 }
 
-class ClusterManager {
-    private static void add() {
-
-    }
-
-    private static void remove() {
-
-    }
-
-    private static void status() {
-
-    }
-}
+//class ClusterManager {
+//    private static void add() {
+//
+//    }
+//
+//    private static void remove() {
+//
+//    }
+//
+//    private static void status() {
+//
+//    }
+//}
